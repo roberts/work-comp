db91ba6e37ec3a8d020fb779daa4da0eda0fca64
drewroberts <github@drewroberts.com>
2021-03-08 13:09:17 -0500

    Create rates migration #4

---
+++ database/migrations/2020_11_06_120000_create_rates_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Roberts\WorkComp\Models\ClassCode;
+
+class CreateRatesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('rates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(ClassCode::class, 'class_code_id');
+            $table->year('year');
+            $table->unsignedInteger('amount'); // In cents.
+            $table->foreignIdFor(app('user'), 'creator_id');
+            $table->foreignIdFor(app('user'), 'updater_id');
+            $table->timestamps();
+        });
+    }
+}
