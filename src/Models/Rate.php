d08a92c1af594383ed4828fe388b80bb7bf9d0c2
drewroberts <github@drewroberts.com>
2021-03-08 13:33:06 -0500

    Add rate factory to model #4

--- src/Models/Rate.php
+++ src/Models/Rate.php
@@ -6,11 +6,12 @@ namespace Roberts\WorkComp\Models;
 
 use Tipoff\Support\Models\BaseModel;
 use Tipoff\Support\Traits\HasCreator;
-// use Tipoff\Support\Traits\HasPackageFactory;
+use Tipoff\Support\Traits\HasPackageFactory;
 use Tipoff\Support\Traits\HasUpdater;
 
 class Rate extends BaseModel
 {
+    use HasPackageFactory;
     use HasCreator;
     use HasUpdater;
 
