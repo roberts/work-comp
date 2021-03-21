f96f56e43a3240fd4a1842e07e044aec842588df
Hunter Montell <64396917+huntermontell@users.noreply.github.com>
2021-03-21 17:32:56 -0500

    Remove import of deleted class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Roberts\WorkComp\Tests;
 
 use Laravel\Nova\NovaCoreServiceProvider;
-use Roberts\WorkComp\Tests\Support\Providers\NovaTestbenchServiceProvider;
 use Roberts\WorkComp\WorkCompServiceProvider;
 use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
+use Tipoff\TestSupport\Providers\NovaPackageServiceProvider;
 
 class TestCase extends BaseTestCase
 {
